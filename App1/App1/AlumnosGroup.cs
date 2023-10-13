38d281e7e8e41255796b90c670c957203d596d15
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2023-10-13 15:21:45 -0500

    actualizando

---
+++ App1/App1/AlumnosGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class AlumnosGroup: List<Alumnos>
+    {
+        public string Heading { get; set; }
+        public List<Alumnos> Alumnos => this;
+    }
+}
