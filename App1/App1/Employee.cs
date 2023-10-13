9ee9e90a95d039e9ec300830a3e30f2760513371
Alumno <Alumno@LAB1509-01>
2023-10-13 15:06:13 -0500

    Agregar archivos de proyecto.

---
+++ App1/App1/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class Employee
+    {
+        public string DisplayName { get; set; }
+    }
+}
